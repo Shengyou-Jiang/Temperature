9269905d7b0e5ac2e5f298a1b7d8363ae61cfe6b
Shengyou Jiang <sjj5492@psu.edu>
2020-09-01 16:44:07 +0000

    added

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+//Author: Shengyou Jiang sjj5492@psu.edu
+//Collaborator: Joel John jmj6182@psu.edu
+//Collabarator: Michael Artlip mva5905@psu.edu
+//Collabarator: Yue Wu yzw5627@psu.edu
+
 #include <stdio.h>
+#include<readline/readline.h>
+#include<stdlib.h>
 
 int main(void) {
-  printf("Hello World\n");
+  char *ctemp = readline("Enter temperature in Celsius: ");
+  double c = atof(ctemp);
+  double f = c * 1.8 + 32;
+  printf("%f° in Celsius is equvalent to %f° Fahrenheit.\n",c, f);
+
   return 0;
 }
